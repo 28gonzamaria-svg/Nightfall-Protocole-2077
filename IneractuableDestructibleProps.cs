ce26758a4bf70e4ec73c171baaf1c49ce9e1d9ee
28gonzamaria-svg <28gonzamaria@gmail.com>
2025-12-23 23:44:07 -0600

    Add Destructible class for object destruction

---
+++ IneractuableDestructibleProps.cs
@@ -0,0 +1,10 @@
+public class Destructible : MonoBehaviour
+{
+    public GameObject destroyedVersion;
+
+    public void DestroyObject()
+    {
+        Instantiate(destroyedVersion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
